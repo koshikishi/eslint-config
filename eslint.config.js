0193e0116818ba9a5d744ac7c84102c1d1b6d651
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2024-01-12 17:09:07 +0300

    chore: set up linters

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import config from '@koshikishi/eslint-config/node';
+import globals from 'globals';
+
+export default [
+  ...config,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
